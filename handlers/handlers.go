9423a9847cfce99e8900d254cb168fadff1c2e00
fulcrum <b.dzikuc@gmail.com>
2024-02-06 09:04:58 +0100

    feat: add yaml file parsing

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -2,22 +2,43 @@ package handlers
 
 import (
 	"html/template"
+	"log"
 	"net/http"
+	"os"
+
+	"gopkg.in/yaml.v3"
 )
 
-type Film struct {
-	Title    string
-	Director string
-}
+// type Film struct {
+// 	Title    string
+// 	Director string
+// }
 
 func DisplayFilms(w http.ResponseWriter, r *http.Request) {
-	tmpl := template.Must(template.ParseFiles("public/index.html"))
-	films := map[string][]Film{
-		"Films": {
-			{Title: "The Godfather", Director: "Francis Ford Coppola"},
-			{Title: "Blade Runner", Director: "Ridley Scott"},
-			{Title: "The Thing", Director: "John Carpenter"},
-		},
+
+	yamlFile, err := os.ReadFile("values.yaml")
+	if err != nil {
+		panic(err)
 	}
-	tmpl.Execute(w, films)
+
+	m := map[interface{}]interface{}{}
+
+	tmpl := template.Must(template.ParseFiles("public/index.html"))
+	err = yaml.Unmarshal([]byte(yamlFile), &m)
+	if err != nil {
+		log.Fatal("error")
+	}
+	tmpl.Execute(w, m)
 }
+
+// func DisplayFilms(w http.ResponseWriter, r *http.Request) {
+// 	tmpl := template.Must(template.ParseFiles("public/index.html"))
+// 	films := map[string][]Film{
+// 		"Films": {
+// 			{Title: "The Godfather", Director: "Francis Ford Coppola"},
+// 			{Title: "Blade Runner", Director: "Ridley Scott"},
+// 			{Title: "The Thing", Director: "John Carpenter"},
+// 		},
+// 	}
+// 	tmpl.Execute(w, films)
+// }
--- main.go
+++ main.go
@@ -3,15 +3,27 @@ package main
 import (
 	"log"
 	"net/http"
+
 	"github.com/fulcrum29/fulcrum/handlers"
 )
 
 func main() {
 
+	// yamlFile, err := os.ReadFile("values.yaml")
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// m := map[interface{}]interface{}{}
+	// films := yaml.Unmarshal([]byte(yamlFile), &m)
+	// if films != nil {
+	// 	log.Fatal("error")
+	// }
+	// fmt.Print(m)
 
 	// define handlers
 	http.HandleFunc("/", handlers.DisplayFilms)
-	//http.HandleFunc("/add-film/", handlers.AddFilm)
+	// //http.HandleFunc("/add-film/", handlers.AddFilm)
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
