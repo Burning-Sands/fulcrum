a0027eec581863c1d54bb8b5a6835de570c49376
fulcrum <b.dzikuc@gmail.com>
2024-03-23 11:06:45 +0100

    feat: add uhc struct, modify page

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -1,44 +1,66 @@
 package handlers
 
 import (
-	"fmt"
 	"html/template"
 	"net/http"
-	"os"
-	"reflect"
 
 	"github.com/fulcrum29/fulcrum/yamleditor"
-	"gopkg.in/yaml.v3"
 )
 
 var yamlEdit yamleditor.YamlOperator
 
+type Values struct {
+	Uhc struct {
+		Replicas string `yaml:"replicas"`
+		Image    struct {
+			Repository string `yaml:"repository"`
+			Tag        string `yaml:"tag"`
+		} `yaml:"image"`
+		Resources struct {
+			Memory struct {
+				Requests int `yaml:"requests"`
+				Limits   int `yaml:"limits"`
+			} `yaml:"memory"`
+		} `yaml:"resources"`
+		Hpa struct {
+			Enabled  bool
+			Replicas struct {
+				Min int `yaml:"min"`
+				Max int `yaml:"max"`
+			} `yaml:"replicas"`
+		} `yaml:"hpa"`
+	} `yaml:"uhc"`
+}
+
+var values Values
+
 func DisplayNodes(w http.ResponseWriter, r *http.Request) {
 
-	if reflect.ValueOf(yamlEdit).IsZero() {
-		file, err := os.ReadFile("values.yaml")
-		if err != nil {
-			panic(err)
-		}
-		yaml.Unmarshal([]byte(file), &yamlEdit.YamlNode)
-	}
-
-	yamlEdit.Buffer.Reset()
-	yamleditor.IterateOverYamlNode(&yamlEdit.YamlNode, &yamlEdit.Buffer)
+	// if reflect.ValueOf(values).IsZero() {
+	// 	file, err := os.ReadFile("values.yaml")
+	// 	if err != nil {
+	// 		panic(err)
+	// 	}
+	// 	yaml.Unmarshal([]byte(file), &values)
+	// }
+	// fmt.Printf("%+v\n", values)
 
 	tmpl := template.Must(template.ParseFiles("public/index.html"))
-	tmpl.Execute(w, yamlEdit.Buffer.String())
+	tmpl.Execute(w, values)
 }
 
 func AddService(w http.ResponseWriter, r *http.Request) {
 
-	yamleditor.IterateOverYamlNode(&yamlEdit.YamlNode, &yamlEdit.Buffer)
-	fmt.Println(yamlEdit.Buffer.String())
-	title := r.PostFormValue("title")
-	director := r.PostFormValue("director")
-	yamleditor.ChangeYamlNodeValue(&yamlEdit.YamlNode, title, director)
-	w.Header().Add("HX-Trigger", "newValue")
+	var (
+		repository = &values.Uhc.Image.Repository
+		tag        = &values.Uhc.Image.Tag
+		replicas   = &values.Uhc.Replicas
+	)
+	*repository = r.PostFormValue("image")
+	*tag = r.PostFormValue("tag")
+	*replicas = r.PostFormValue("replicas")
 
+	w.Header().Add("HX-Trigger", "valuesChanged")
 	// tmpl := template.Must(template.ParseFiles("public/index.html"))
 	// tmpl.ExecuteTemplate(w, "film-list-element", m)
 }
--- main.go
+++ main.go
@@ -8,16 +8,19 @@ import (
 )
 
 func main() {
+	// playground
+	// var valuesFile yamleditor.Values
+	// file, err := os.ReadFile("values.yaml")
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// yaml.Unmarshal([]byte(file), &valuesFile)
+	// fmt.Printf("%+v\n", valuesFile)
 
 	// define handlers
 	http.HandleFunc("/", handlers.DisplayNodes)
 	http.HandleFunc("/add-service/", handlers.AddService)
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
-
-	// f, err := os.OpenFile("values.yaml", os.O_RDWR|os.O_CREATE, 0644)
-	// if err != nil {
-	// 	panic(err)
-	// }
 
 }
