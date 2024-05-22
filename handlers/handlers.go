cca6736fb73dd5fa3e33d82a4a34a21d7609bc13
Bartosz <bartosz.dzikuc@grid.gg>
2024-05-22 20:13:18 +0200

    feat: add new values struct

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -5,107 +5,140 @@ import (
 	"html/template"
 	"net/http"
 	"os"
+	"strconv"
 
 	// "github.com/fulcrum29/fulcrum/yamleditor"
 	"gopkg.in/yaml.v3"
 )
 
 type Values struct {
-	Uhc struct {
-		Replicas string `yaml:"replicas"`
-		Image    struct {
-			Repository string `yaml:"repository"`
-			Tag        string `yaml:"tag"`
-		} `yaml:"image"`
-		Resources struct {
-			Memory struct {
-				Requests int `yaml:"requests"`
-				Limits   int `yaml:"limits"`
-			} `yaml:"memory"`
-		} `yaml:"resources"`
-		Hpa struct {
-			Enabled  bool
-			Replicas struct {
-				Min int `yaml:"min"`
-				Max int `yaml:"max"`
-			} `yaml:"replicas"`
-		} `yaml:"hpa"`
-	} `yaml:"uhc"`
+	NameOverride     string `yaml:"nameOverride"`
+	FullnameOverride string `yaml:"fullnameOverride"`
+	ReplicaCount     int    `yaml:"replicaCount"`
+	Annotations      struct {
+	} `yaml:"annotations"`
+	PodAnnotations struct {
+	} `yaml:"podAnnotations"`
+	Image struct {
+		Repository string `yaml:"repository"`
+		Tag        string `yaml:"tag"`
+		PullPolicy string `yaml:"pullPolicy"`
+	} `yaml:"image"`
+	Env       []interface{} `yaml:"env"`
+	EnvFrom   []interface{} `yaml:"envFrom"`
+	Resources struct {
+		Limits struct {
+			CPU    string `yaml:"cpu"`
+			Memory string `yaml:"memory"`
+		} `yaml:"limits"`
+		Requests struct {
+			CPU    string `yaml:"cpu"`
+			Memory string `yaml:"memory"`
+		} `yaml:"requests"`
+	} `yaml:"resources"`
+	VolumeMounts interface{} `yaml:"volumeMounts"`
+	Affinity     struct {
+		NodeAffinity struct {
+			RequiredDuringSchedulingIgnoredDuringExecution struct {
+				NodeSelectorTerms []struct {
+					MatchExpressions []struct {
+						Key      string   `yaml:"key"`
+						Operator string   `yaml:"operator"`
+						Values   []string `yaml:"values"`
+					} `yaml:"matchExpressions"`
+				} `yaml:"nodeSelectorTerms"`
+			} `yaml:"requiredDuringSchedulingIgnoredDuringExecution"`
+		} `yaml:"nodeAffinity"`
+	} `yaml:"affinity"`
+	Tolerations               []interface{} `yaml:"tolerations"`
+	TopologySpreadConstraints []interface{} `yaml:"topologySpreadConstraints"`
+	Ports                     []interface{} `yaml:"ports"`
+	Service                   struct {
+		Ports []struct {
+			Name       string `yaml:"name"`
+			TargetPort string `yaml:"targetPort"`
+			Protocol   string `yaml:"protocol"`
+			Port       int    `yaml:"port"`
+		} `yaml:"ports"`
+	} `yaml:"service"`
+	Metrics struct {
+		Enabled        bool `yaml:"enabled"`
+		ServiceMonitor struct {
+			Enabled bool `yaml:"enabled"`
+		} `yaml:"serviceMonitor"`
+	} `yaml:"metrics"`
+	Ingress struct {
+	} `yaml:"ingress"`
+	Hpa struct {
+		Enabled     bool          `yaml:"enabled"`
+		MinReplicas int           `yaml:"minReplicas"`
+		MaxReplicas int           `yaml:"maxReplicas"`
+		Metrics     []interface{} `yaml:"metrics"`
+	} `yaml:"hpa"`
+	PodDisruptionBudget struct {
+		Enabled      bool `yaml:"enabled"`
+		MinAvailable int  `yaml:"minAvailable"`
+	} `yaml:"podDisruptionBudget"`
 }
 
 func NewValues() *Values {
-  return &Values{}
+	return &Values{}
 }
 
 func DisplayIndex(values *Values) http.Handler {
 
-  fn := func(w http.ResponseWriter, r *http.Request) {
+	fn := func(w http.ResponseWriter, r *http.Request) {
 
-    fmt.Println(values)
-    tmpl := template.Must(template.ParseFiles("public/index.html"))
-    tmpl.ExecuteTemplate(w, "index", *values)
-  }
+		tmpl := template.Must(template.ParseFiles("public/index.html"))
+		tmpl.ExecuteTemplate(w, "index", *values)
+	}
 
-  return http.HandlerFunc(fn)
+	return http.HandlerFunc(fn)
 }
 
 func DisplayValues(values *Values) http.Handler {
 
-  fn := func(w http.ResponseWriter, r *http.Request) {
+	fn := func(w http.ResponseWriter, r *http.Request) {
 
-    tmpl := template.Must(template.ParseFiles("public/index.html"))
-    tmpl.ExecuteTemplate(w, "display-values", *values)
-  }
-  return http.HandlerFunc(fn)
+		tmpl := template.Must(template.ParseFiles("public/index.html"))
+		tmpl.ExecuteTemplate(w, "display-values", &values)
+		fmt.Println("Display", values)
+	}
+	return http.HandlerFunc(fn)
 }
 
 func (v *Values) ModifyValues() http.Handler {
 
-  fn := func(w http.ResponseWriter, r *http.Request) {
+	fn := func(w http.ResponseWriter, r *http.Request) {
 
-    var (
-      repository = &v.Uhc.Image.Repository
-      tag        = &v.Uhc.Image.Tag
-      replicas   = &v.Uhc.Replicas
-    )
-    *repository = r.PostFormValue("image")
-    *tag        = r.PostFormValue("tag")
-    *replicas   = r.PostFormValue("replicas")
+		var (
+			repository = &v.Image.Repository
+			tag        = &v.Image.Tag
+			replicas   = &v.ReplicaCount
+		)
+		*repository = r.PostFormValue("image")
+		*tag = r.PostFormValue("tag")
+		*replicas, _ = strconv.Atoi(r.PostFormValue("replicas"))
 
-
-    w.Header().Add("HX-Trigger", "valuesChanged")
-  }
-  return http.HandlerFunc(fn)
+		fmt.Println("Modify", v)
+		w.Header().Add("HX-Trigger", "valuesChanged")
+	}
+	return http.HandlerFunc(fn)
 }
-
 
 func ApplyValues(values *Values) http.Handler {
 
-  fn := func(w http.ResponseWriter, r *http.Request) {
-    
-    fileName := "values-output.yaml"
-    writer, err := os.Create(fileName)
-    if err != nil {
-      panic("Unable to create the output file")
-    }
-    encoder := yaml.NewEncoder(writer)
-    encoder.SetIndent(2)
-    encoder.Encode(*values)
-    encoder.Close()
-  }
-  return http.HandlerFunc(fn)
+	fn := func(w http.ResponseWriter, r *http.Request) {
+
+		fileName := "values-output.yaml"
+		writer, err := os.Create(fileName)
+		if err != nil {
+			panic("Unable to create the output file")
+		}
+		encoder := yaml.NewEncoder(writer)
+		encoder.SetIndent(2)
+		encoder.Encode(*values)
+		encoder.Close()
+	}
+	return http.HandlerFunc(fn)
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- main.go
+++ main.go
@@ -6,26 +6,16 @@ import (
 	"github.com/fulcrum29/fulcrum/handlers"
 )
 
-func main() {
-  // values := handlers.NewValues()
-	// playground
-	// var valuesFile yamleditor.Values
-	// file, err := os.ReadFile("values.yaml")
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// yaml.Unmarshal([]byte(file), &valuesFile)
-	// fmt.Printf("%+v\n", valuesFile)
 
+func main() {
+  
   values := handlers.Values{}
-  di := handlers.DisplayIndex(&values)
-  // fmt.Print(values)
   // fs := http.FileServer(http.Dir("./view"))
 	// define handlers
   router := http.NewServeMux()
-	router.Handle("/", di) 
-	router.Handle("/display-values/", handlers.DisplayValues(&values))
+	router.Handle("/", handlers.DisplayIndex(&values)) 
   router.Handle("/edit/", values.ModifyValues())
+	router.Handle("/display-values/", handlers.DisplayValues(&values))
   router.Handle("/apply/", handlers.ApplyValues(&values))
 	log.Fatal(http.ListenAndServe(":8080", router))
 
