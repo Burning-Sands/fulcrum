ae1a89f82c532cea2bf34b9a30b611598d22bb31
fulcrum <b.dzikuc@gmail.com>
2024-05-27 23:04:06 +0200

    feat: add separate routes

---
+++ cmd/routes.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+  "net/http"
+)
+
+func (app *application) routes() *http.ServeMux {
+
+	router := http.NewServeMux()
+	fs := http.FileServer(http.Dir("./ui/static"))
+
+	// define handlers
+  router.Handle("GET /static/", http.StripPrefix("/static", fs))
+	router.Handle("GET /{$}", app.DisplayIndex()) 
+  router.Handle("POST /edit", app.ModifyValues())
+	router.Handle("GET /display-values", app.DisplayValues())
+  router.Handle("GET /apply", app.ApplyValues())
+  
+  return router
+}
